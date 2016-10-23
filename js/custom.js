a155e45ddf66d816f8b70badefe4f86bbd845177
Matthew Gordon <laxredemption5525@gmail.com>
2016-10-23 15:57:39 -0600

    Updated form to reset upon successful complete order

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
   var shoppingCart = $('#shopping_cart>tbody');
   var cartSubTotal = $('#shopping_cart>tfoot>tr#subTotal');
+  var custInfo = $('#orderForm');
   var submitButton = $(':button#submitOrder');
   var subTotal = 0;
   var tax = $('#shopping_cart>tfoot>tr#tax');
@@ -11,10 +12,31 @@ $(document).ready(function(){
   var swine = $("div#swine>a");
   var iceCream = $("div#iceCream>a");
   var menuItems = {
-    royale : { name : 'Royale with Cheese', price : 8.99 },
-    arugala : { name : 'Arugala Pizza', price : 11.99 },
-    swine : { name : 'Smoked Swine', price : 14.99 },
-    iceCream : { name : 'Ice Cream Biscuit', price : 7.99 }
+    royale : {
+       name : 'Royale with Cheese',
+       price : 8.99
+      },
+    arugala : {
+       name : 'Arugala Pizza',
+       price : 11.99
+      },
+    swine : {
+      name : 'Smoked Swine',
+      price : 14.99
+      },
+    iceCream : {
+      name : 'Ice Cream Biscuit',
+      price : 7.99
+    }
+  }
+
+  // update subtotal, tax, total
+  function updatePrices() {
+    var tax = Math.round((subTotal * 0.028) * 100) / 100;
+    var total = subTotal + tax;
+    $('#subTotal').text('$ ' + subTotal.toFixed(2));
+    $('#tax').text('$ ' + tax.toFixed(2));
+    $('#total').text('$ ' + total.toFixed(2));
   }
 
   // add items to cart
@@ -43,15 +65,6 @@ $(document).ready(function(){
       updatePrices();
       });
 
-  // update subtotal, tax, total
-  function updatePrices() {
-    var tax = Math.round((subTotal * 0.028) * 100) / 100;
-    var total = subTotal + tax;
-    $('#subTotal').text('$ ' + subTotal.toFixed(2));
-    $('#tax').text('$ ' + tax.toFixed(2));
-    $('#total').text('$ ' + total.toFixed(2));
-  }
-
   // validate submit form
   submitButton.click(function(){
     var fullName = $(':input.fullName');
@@ -77,9 +90,12 @@ $(document).ready(function(){
         address.addClass('invalid');
         Materialize.toast('Please enter a valid address.', 3000);
       } else {
-        fullName.addClass('valid');
-        phoneNumber.addClass('valid');
-        address.addClass('valid');
+        shoppingCart.empty();
+        orderForm.reset();
+        subTotal = 0;
+        $('#subTotal').empty();
+        $('#tax').empty();
+        $('#total').empty();
         Materialize.toast('Order Successfully submitted', 3000);
       }
     }
